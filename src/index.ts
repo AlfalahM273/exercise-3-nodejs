c55639ac850994adb7f00bac79f28f31eddba416
AlfalahM273 <MuhammadAlfalah.Madukubah@mitrais.com>
2021-09-10 09:27:27 +0800

    chore: public path

--- src/index.ts
+++ src/index.ts
@@ -14,13 +14,12 @@ app.use(fileUpload({
     createParentPath: true
 }));
 
-console.log('__public', path.join(__dirname, 'public'));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/../public')));
 
 // define a route handler for the default home page
 app.get('/', (req, res) => {
